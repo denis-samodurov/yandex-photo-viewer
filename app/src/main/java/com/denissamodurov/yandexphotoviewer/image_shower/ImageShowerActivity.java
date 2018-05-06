ad2949dee027219be81518b19de54f4436ff6d24
denis-samodurov <samodurov.v.denis@gmail.com>
2018-05-06 22:34:19 +0300

    Добавил получение картинки из Intent

--- app/src/main/java/com/denissamodurov/yandexphotoviewer/image_shower/ImageShowerActivity.java
+++ app/src/main/java/com/denissamodurov/yandexphotoviewer/image_shower/ImageShowerActivity.java
@@ -11,13 +11,21 @@ import com.denissamodurov.yandexphotoviewer.R;
 
 public class ImageShowerActivity extends AppCompatActivity {
     private static final String BITMAP_IMAGE = "BITMAP_IMAGE";
-
+    private Bitmap imageToShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_shower);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getDataFromIntent(getIntent());
+    }
+
+    private void getDataFromIntent(Intent intent) {
+        if(intent.hasExtra(BITMAP_IMAGE)){
+            imageToShow = intent.getParcelableExtra(BITMAP_IMAGE);
+        }
     }
 
     public static Intent getIntent(Context context, Bitmap bitmap) {
